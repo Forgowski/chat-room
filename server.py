429a2c909b6b04754d894ce641158145304e18d7
Krzysztof Tyszkiewicz <rysiu352@gmail.com>
2023-01-17 22:32:09 +0100

    handle function

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket
-from threading import Thread
+import threading
 
 
 HOST = "127.0.0.1"
@@ -13,9 +13,26 @@ server.listen()
 clients = []
 nicknames = []
 
+
 def broadcast(message):
     for client in clients:
         client.send(message)
+
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            print(f"{nicknames[clients.index(client)]}")
+            broadcast(message)
+        except:
+            index = client.index(client)
+            client.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            break
+
 
 def receive():
     while True:
@@ -26,4 +43,10 @@ def receive():
         nicknames.append(nickname)
         clients.append(client)
         client.send("connected to the server".encode("utf-8"))
-        
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+print("server running")
+receive()
