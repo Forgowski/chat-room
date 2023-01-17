17f32bf66b221907870df603c7b110a1594d7b59
Krzysztof Tyszkiewicz <rysiu352@gmail.com>
2023-01-17 22:24:53 +0100

    server broadcast and recaive function

--- server.py
+++ server.py
@@ -2,4 +2,28 @@ import socket
 from threading import Thread
 
 
+HOST = "127.0.0.1"
+PORT = 9090
 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}!")
+        client.send("NICK".encode('utf-8'))
+        nickname = client.recv(1024)
+        nicknames.append(nickname)
+        clients.append(client)
+        client.send("connected to the server".encode("utf-8"))
+        
