fab622c17a5e671e3162040c1c0a23bcfd87e6cc
Krzysztof Tyszkiewicz <rysiu352@gmail.com>
2023-01-20 01:39:21 +0100

    password different

--- register.py
+++ register.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 
 class Register:
@@ -6,24 +7,30 @@ class Register:
         self.win = Tk()
         self.win.configure(bg="lightgray")
         self.l1 = Label(text="Login", bg="lightgray")
-        self.l1.pack()
+        self.l1.pack(padx=20, pady=1)
 
         self.login = Entry()
-        self.login.pack()
+        self.login.pack(padx=20, pady=5)
 
         self.l2 = Label(text="Password", bg="lightgray")
-        self.l2.pack()
+        self.l2.pack(padx=20, pady=1)
 
         self.password = Entry()
-        self.password.pack()
+        self.password.pack(padx=20, pady=5)
 
         self.l3 = Label(text="Repeat password", bg="lightgray")
-        self.l3.pack()
+        self.l3.pack(padx=20, pady=1)
 
         self.password_repeated = Entry()
-        self.password_repeated.pack()
+        self.password_repeated.pack(padx=20, pady=5)
 
+        self.submit = Button(text="register", command=self.check_data)
+        self.submit.pack(padx=20, pady=20)
         self.win.mainloop()
+
+    def check_data(self):
+        if self.password.get() != self.password_repeated.get():
+            messagebox.showerror(title="Error", message="Passwords are different")
 
 
 Register()
